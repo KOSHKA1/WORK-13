855550794d1d0f19e01570f44442760ba37abf59
KOSHKA1 <100477503+KOSHKA1@users.noreply.github.com>
2022-06-21 16:44:07 +0400

    java.js

---
+++ work.js
@@ -0,0 +1,15 @@
+let userName = 'ZUKA';
+
+let userSurname = "muskhelishvili";
+let age = 20;
+let userGmail = "zukamusxelishvili2004@gmail.com";
+let lokationOfuser = 'georgia';
+
+
+let nickName = "musxela";
+let birthday = "17/04/2004";
+
+
+
+let text = "my name is" + userName + "i am " + "my nickName is" + nickName;
+let text2 = `my name is ${userName} my nickName is ${nickName}`;
